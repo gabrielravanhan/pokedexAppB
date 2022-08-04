2b489ea720ad299c34732a23c0913aadc6c3e201
gabrielravanhan <gabrielravanhanjose@gmail.com>
2022-08-04 15:02:52 -0300

    1 file modified

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,7 @@ import { PokemonService } from '../services/pokemon.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  
+
   total = 0;
   next = '';
   previous = '';
@@ -16,12 +16,22 @@ export class HomePage implements OnInit {
   constructor(public pokemonService: PokemonService) {}
 
   ngOnInit() {
+    this.buscarPokemon();
+  }
+
+  async buscarPokemon() {
     this.pokemonService.buscarTodosPokemon().subscribe((dados) => {
       this.total = dados['count'];
       this.next = dados['next'];
       this.previous = dados['previous'];
-      this.listaPokemon = dados['results'];
+      //this.listaPokemon = dados['results'];
       console.log(dados);
+
+      for (let pokemon of dados['results']) {
+        this.pokemonService.buscarUmPokemon(pokemon.url).subscribe(dadosPokemon => {
+          this.listaPokemon.push(dadosPokemon);
+        });
+      }
     });
   }
 }
